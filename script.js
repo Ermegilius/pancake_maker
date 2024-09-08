bec7b5020dc65287e2a1df6a01523c7a0025b367
Beliakov Vladimir <s2400780@BC-C02J63G1Q6LRs-MacBook-Air.local>
2024-09-08 14:18:16 +0300

    initial commit. Do all tasks excepting animations and media queries

---
+++ script.js
@@ -0,0 +1,29 @@
+let totalPriceBanner = document.querySelectorAll("#totalPrice");
+//console.log(totalPriceBanner);
+//let totalPriceBottom = document.querySelector("#totalPriceBottom");
+
+function totalCounter() {
+    //console.log(chosenTypePrice, typeof (chosenTypePrice));
+
+    let chosenAdds = document.getElementsByTagName('input');
+    //console.log(chosenAdds);
+    //console.log("The first checkbox's value is:" + chosenAdds[0].value);
+    let chosenAddsPrice = 0;
+    for (let i = 0; i < chosenAdds.length; i++) {
+        let checkedValue = 0;
+        if (chosenAdds[i].checked) {
+            checkedValue = +chosenAdds[i].value;
+            chosenAddsPrice += checkedValue;
+        }
+    }
+    let chosenType = document.querySelector("#type");
+    let chosenTypePrice = +chosenType.value;
+    //console.log("Chosen adds price is " + chosenAddsPrice);
+    totalPrice = chosenTypePrice + chosenAddsPrice;
+    //console.log("Total price is " + totalPrice);
+    for (i = 0; i < totalPriceBanner.length; i++) {
+        totalPriceBanner[i].textContent = "$" + totalPrice;
+    }
+}
+
+addEventListener('change', totalCounter);
