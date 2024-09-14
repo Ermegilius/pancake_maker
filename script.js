f77ef5beb5c00717ff1b601f0b95a3690bcb7d7b
Beliakov Vladimir <ermegilius@gmail.com>
2024-09-14 17:00:05 +0300

    end of the day, 14.09.2024. Finish pancake project: order form and css finetune.

--- script.js
+++ script.js
@@ -3,22 +3,20 @@ let totalPriceBanner = document.querySelectorAll("#totalPrice");
 //let totalPriceBottom = document.querySelector("#totalPriceBottom");
 
 const clientName = document.querySelector('#clientName');
-let formHeader = document.querySelector('#formHeader');
-let customizationSection = document.querySelectorAll('.customization-section');
+let formHeader = document.querySelector('#formHeader');//changes from "custimize" to "your order"
+let customizationSection = document.querySelectorAll('.customization-section');//whole form, hides after order button
 let container = document.querySelector('.form-container');
-let order = document.querySelector('#order');
+let order = document.querySelector('#order');//whole order form, hidden initially, appears after the order button
 
-let orderClientName = document.querySelector('#orderClientName');
-let orderPancaceOptions = document.querySelector('#orderPancaceOptions');
-let orderDelivery = document.querySelector('#orderDelivery');
-let orderTotalPrice = document.querySelector('#orderTotalPrice');
+//here are the fields from the order form (appears after the order button):
+let orderClientName = document.querySelector('#orderClientName');//client's name
+let orderPancakeOptions = document.querySelector('#orderPancakeOptions'); // all pankake's options
+let orderDelivery = document.querySelector('#orderDelivery');//chosen delivery option
+let orderTotalPrice = document.querySelector('#orderTotalPrice');  //total price
 
+//this function returns the totall price of the order
 function totalCounter() {
-    //console.log(chosenTypePrice, typeof (chosenTypePrice));
-
-    let chosenAdds = document.getElementsByTagName('input');
-    //console.log(chosenAdds);
-    //console.log("The first checkbox's value is:" + chosenAdds[0].value);
+    let chosenAdds = document.getElementsByTagName('input'); //all inputs on the page
     let chosenAddsPrice = 0;
     for (let i = 0; i < chosenAdds.length; i++) {
         let checkedValue = 0;
@@ -32,12 +30,13 @@ function totalCounter() {
     let chosenTypePrice = +chosenType.value;
     //console.log("Chosen adds price is " + chosenAddsPrice);
     totalPrice = chosenTypePrice + chosenAddsPrice;
-    console.log("Total price is " + totalPrice);
+    // console.log("Total price is " + totalPrice);
     for (i = 0; i < totalPriceBanner.length; i++) {
         totalPriceBanner[i].textContent = "$" + totalPrice;
     }
 }
 
+//this is a price-banner animation
 function priceBannerAnimation() {
     let banner = document.querySelector('.price-banner');
     banner.style.animationName = 'priceBannerAnimation';
@@ -48,15 +47,46 @@ function priceBannerAnimation() {
     }, 1000);
 }
 
+//hides order customization and unhides order info
 function placeOrder() {
-    orderClientName.textContent = clientName.value;
+    if (clientName.value.trim() === '') {
+        alert("Please write your name.");
+    } else {
+        //name section:
+        orderClientName.textContent = clientName.value; //insert clients name from input
 
-    formHeader.textContent = 'Your order is: ';
-    order.classList.remove('hidden'); // show order details
+        //pancake options section:
+        //type option:
+        let chosenType = document.querySelector("#type");
+        let chosenAdds = document.querySelectorAll('input[type="checkbox"]:checked');
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(`${chosenType.options[chosenType.selectedIndex].textContent}`));
+        orderPancakeOptions.appendChild(li);
 
-    customizationSection.forEach((section) => {
-        section.classList.add('hidden');
-    }) //hide all sustomization fields
+        //toppings and extras options:
+        for (let i = 0; i < chosenAdds.length; i++) {
+            if (chosenAdds[i]) {
+                let li = document.createElement("li");
+                li.appendChild(document.createTextNode(chosenAdds[i].nextSibling.textContent));
+                orderPancakeOptions.appendChild(li);
+            }
+        }
+
+        // orderPancakeOptions.textContent = ;
+        let selectedDelivery = document.querySelector('input[name="deliveryInfo"]:checked');//get checked delivery input
+        let selectedDeliveryLabel = document.querySelector(`label[for="${selectedDelivery.id}"]`);//get label of the checked delivery input
+        orderDelivery.textContent = selectedDeliveryLabel.textContent;//change field to the text content of the label of the checked delivery input
+        orderTotalPrice.textContent = `$${totalPrice}`;
+
+        // show order details
+        formHeader.textContent = 'Your order is: ';
+        order.classList.remove('hidden');
+
+        //hide all sustomization fields
+        customizationSection.forEach((section) => {
+            section.classList.add('hidden');
+        })
+    }
 }
 
 addEventListener('change', totalCounter);
