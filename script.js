7b5ab54c4137999cd929a7f57e06041a93fdb331
Beliakov Vladimir <s2400780@BC-C02J63G1Q6LRs-MacBook-Air.local>
2024-09-14 12:59:32 +0300

    start of the day 14.09.2024

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ let totalPriceBanner = document.querySelectorAll("#totalPrice");
 //console.log(totalPriceBanner);
 //let totalPriceBottom = document.querySelector("#totalPriceBottom");
 
+const clientName = document.querySelector('#clientName');
+let formHeader = document.querySelector('#formHeader');
+let customizationSection = document.querySelectorAll('.customization-section');
+let container = document.querySelector('.form-container');
+let order = document.querySelector('#order');
+
+let orderClientName = document.querySelector('#orderClientName');
+let orderPancaceOptions = document.querySelector('#orderPancaceOptions');
+let orderDelivery = document.querySelector('#orderDelivery');
+let orderTotalPrice = document.querySelector('#orderTotalPrice');
+
 function totalCounter() {
     //console.log(chosenTypePrice, typeof (chosenTypePrice));
 
@@ -27,10 +38,25 @@ function totalCounter() {
 }
 
 function priceBannerAnimation() {
-
+    let banner = document.querySelector('.price-banner');
+    banner.style.animationName = 'priceBannerAnimation';
+    banner.style.backgroundImage = 'url(./public/images/Spinner@1x-1.0s-200px-200px.gif)';
+    setTimeout(() => {
+        banner.style.animationName = '';
+        banner.style.backgroundImage = '';
+    }, 1000);
 }
 
+function placeOrder() {
+    orderClientName.textContent = clientName.value;
 
+    formHeader.textContent = 'Your order is: ';
+    order.classList.remove('hidden'); // show order details
+
+    customizationSection.forEach((section) => {
+        section.classList.add('hidden');
+    }) //hide all sustomization fields
+}
 
 addEventListener('change', totalCounter);
 addEventListener('change', priceBannerAnimation);
